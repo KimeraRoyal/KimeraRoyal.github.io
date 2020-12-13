78644fcfe85dfba964a906f384129b79c8af5d53
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2020-12-13 16:34:17 +0000

    Improved Background Reactivity
    Canvas background animation now reacts to the mouse as well as the previous scroll wheel reactivity. Additionally, the background now uses a delta timer to ensure smooth, consistent movement regardless of FPS

--- www/js/script.js
+++ www/js/script.js
@@ -4,8 +4,14 @@ var bgCanvas;
 var bgContext;
 var optimalHeight = 722;
 
-var scrollProportion = 0;
-var currentScrollOffset = 0;
+/* Helper Functions */
+
+function lerp(a, b, amount)
+{
+  return a + (b - a) * amount;
+}
+
+/* BG Particles */
 
 var bgParticles = {};
 var bgParticleIndex = 0;
@@ -15,18 +21,20 @@ var currentBgParticles = 0;
 
 var bgParticleProperties =
 {
-  prewarm: 200,
+  maxParticles: 200,
+  spawnInterval: 3,
+  spawnAmount: 30,
 
-  maxParticles: 150,
-  spawnInterval: 2,
-  spawnAmount: 4,
-
-  minXSpeed: 2, maxXSpeed: 3,
-  minYSpeed: 2, maxYSpeed: 5,
+  minXSpeed: 4, maxXSpeed: 8,
+  minYSpeed: 6, maxYSpeed: 10,
   particleSize: 3,
 
-  maxLife: 300,
+  minTotalLife: 150,
+  maxTotalLife: 700,
+  lifeIncrementSlow: 1,
+  lifeIncrementFast: 10,
 
+  mouseMoveOffset: 50,
   scrollOffset: 500
 };
 
@@ -37,10 +45,15 @@ function BGParticle()
     this.x = (Math.random() * (bgCanvas.width + bgCanvas.height)) - bgCanvas.height;
     this.y = 0;
 
-    this.startingOffset = currentScrollOffset;
-
     this.xSpeed = (Math.random() * (bgParticleProperties.maxXSpeed - bgParticleProperties.minXSpeed)) + bgParticleProperties.minXSpeed;
     this.ySpeed = (Math.random() * (bgParticleProperties.maxYSpeed - bgParticleProperties.minYSpeed)) + bgParticleProperties.minYSpeed;
+
+    this.startingMouseProportionX = mouseProportionX;
+    this.startingMouseProportionY = mouseProportionY;
+    this.startingScrollOffset = currentScrollOffset;
+
+    this.displayX = this.x;
+    this.displayY = this.y;
 
     bgParticleIndex++;
     currentBgParticles++;
@@ -49,16 +62,25 @@ function BGParticle()
     this.id = bgParticleIndex;
 
     this.currentLife = 0;
+    this.totalLife = (Math.random() * (bgParticleProperties.maxTotalLife - bgParticleProperties.minTotalLife)) + bgParticleProperties.minTotalLife;
   }
 }
 
 BGParticle.prototype.update = function()
 {
-  this.x += this.xSpeed;
-  this.y += this.ySpeed;
+  this.x += this.xSpeed * deltaTime;
+  this.y += this.ySpeed * deltaTime;
 
-  this.currentLife++;
-  if(this.x > bgCanvas.width || this.y > bgCanvas.height || this.currentLife >= bgParticleProperties.maxLife)
+  if(this.x > bgCanvas.width || this.y > bgCanvas.height)
+  {
+    this.currentLife += bgParticleProperties.lifeIncrementFast;
+  }
+  else
+  {
+    this.currentLife += bgParticleProperties.lifeIncrementSlow;
+  }
+
+  if(this.currentLife >= this.totalLife)
   {
     currentBgParticles--;
     delete bgParticles[this.id];
@@ -68,68 +90,16 @@ BGParticle.prototype.update = function()
 BGParticle.prototype.draw = function()
 {
 
-  var lifetimePercentage = this.currentLife / bgParticleProperties.maxLife;
+  var lifetimePercentage = this.currentLife / this.totalLife;
 
   bgContext.fillStyle = "rgba(255, 255, 255, " +  (1 - lifetimePercentage) + ")";
-  bgContext.fillRect(this.x, this.y - (currentScrollOffset - this.startingOffset), bgParticleProperties.particleSize, bgParticleProperties.particleSize);
-}
 
-function onLoad()
-{
-  body = document.getElementById("body");
-  body.onscroll = scrollBody;
+  var displayX = this.x + ((mouseProportionX - this.startingMouseProportionX) * bgParticleProperties.mouseMoveOffset);
+  var displayY = this.y + ((mouseProportionY - this.startingMouseProportionY) * bgParticleProperties.mouseMoveOffset) - (currentScrollOffset - this.startingScrollOffset);
 
-  bgCanvas = document.getElementById("background-particles");
-
-  setCanvasSize();
-
-  bgContext = bgCanvas.getContext("2d");
-
-  loadCanvas();
-}
-
-function scrollBody()
-{
-  var maxScroll = body.scrollHeight - body.clientHeight;
-  if(maxScroll > 0)
-  {
-    scrollProportion = body.scrollTop / maxScroll;
-  }
-  else
-  {
-    scrollProportion = 0;
-  }
-    console.log(currentScrollOffset);
-
-  scrollProportion = Math.min(Math.max(scrollProportion, 0), 1);
-  currentScrollOffset = bgParticleProperties.scrollOffset * scrollProportion;
-}
-
-function loadCanvas()
-{
-  bgContext.fillStyle = "rgba(0, 0, 0, 0)";
-  bgContext.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
-
-  for(var i = 0; i < bgParticleProperties.prewarm; i++)
-  {
-    updateBGParticles()
-  }
-
-  updateCanvas();
-}
-
-function updateCanvas()
-{
-  bgContext.clearRect(0, 0, bgCanvas.width, bgCanvas.height);
-
-  updateBGParticles();
-
-  for(var i in bgParticles)
-  {
-    bgParticles[i].draw();
-  }
-
-  window.requestAnimationFrame(updateCanvas);
+  this.displayX = lerp(this.displayX, displayX, 0.1);
+  this.displayY = lerp(this.displayY, displayY, 0.1);
+  bgContext.fillRect(this.displayX, this.displayY, bgParticleProperties.particleSize, bgParticleProperties.particleSize);
 }
 
 function updateBGParticles()
@@ -149,10 +119,105 @@ function updateBGParticles()
   }
 }
 
+/* Delta Time */
+
+var startTime = 0;
+var lastTime = 0;
+
+var totalTime = 0;
+var deltaTime = 1;
+
+function initializeDeltaTime()
+{
+  startTime = Date.now();
+}
+
+function calculateDeltaTime()
+{
+  var currentTime = Date.now();
+  totalTime = currentTime - startTime;
+
+  deltaTime = (currentTime - lastTime) / 60;
+
+  lastTime = currentTime;
+}
+
+/* Canvas */
+
+function loadCanvas()
+{
+  bgContext.fillStyle = "rgba(0, 0, 0, 0)";
+  bgContext.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
+
+  updateCanvas();
+}
+
+function updateCanvas()
+{
+  bgContext.clearRect(0, 0, bgCanvas.width, bgCanvas.height);
+
+  calculateDeltaTime();
+
+  updateBGParticles();
+  for(var i in bgParticles)
+  {
+    bgParticles[i].draw();
+  }
+
+  window.requestAnimationFrame(updateCanvas);
+}
+
 function setCanvasSize()
 {
   bgCanvas.width = window.innerWidth / (window.innerHeight / optimalHeight);
   bgCanvas.height = optimalHeight;
+}
+
+/* Window Events */
+
+var mouseProportionX = 0;
+var mouseProportionY = 0;
+
+var scrollProportion = 0;
+var currentScrollOffset = 0;
+
+function onMouseMove(event)
+{
+  mouseProportionX = event.pageX / window.innerWidth;
+  mouseProportionY = event.pageY / window.innerHeight;
+}
+
+function scrollBody(event)
+{
+  var maxScroll = body.scrollHeight - body.clientHeight;
+  if(maxScroll > 0)
+  {
+    scrollProportion = body.scrollTop / maxScroll;
+  }
+  else
+  {
+    scrollProportion = 0;
+  }
+
+  scrollProportion = Math.min(Math.max(scrollProportion, 0), 1);
+  currentScrollOffset = bgParticleProperties.scrollOffset * scrollProportion;
+}
+
+function onLoad()
+{
+  document.onmousemove = onMouseMove;
+
+  body = document.getElementById("body");
+  body.onscroll = scrollBody;
+
+  bgCanvas = document.getElementById("background-particles");
+
+  setCanvasSize();
+
+  bgContext = bgCanvas.getContext("2d");
+
+  loadCanvas();
+  initializeDeltaTime();
 }
 
 window.onload = onLoad;
